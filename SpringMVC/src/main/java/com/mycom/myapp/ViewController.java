dd6eb9f9c7eaf9c53bda6c876b67038c387747ca
Min kyungdae <rudwl1005@naver.com>
2022-04-19 13:51:06 +0900

    2022-04-19 Spring (ModelAndView)

--- SpringMVC/src/main/java/com/mycom/myapp/ViewController.java
+++ SpringMVC/src/main/java/com/mycom/myapp/ViewController.java
@@ -3,6 +3,7 @@ package com.mycom.myapp;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.mycom.myapp.dto.EmpDto;
 
@@ -27,5 +28,21 @@ public class ViewController {
 		model.addAttribute("seq", "12345");
 		model.addAttribute("empDto", new EmpDto(1600, "박", "찬호", "park@park.com", "2000-01-01"));
 		return "viewTest3";
+	}
+	
+	@GetMapping(value = "/viewTest4")
+	public ModelAndView viewTest4() { // 파라미터로 Model 받지 않음
+		// 데이터와 jsp를 동시에 구성
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("seq", "12345");
+		mav.addObject("empDto", new EmpDto(1600, "박", "찬호", "park@park.com", "2000-01-01"));
+		mav.setViewName("viewTest4");
+		return mav;
+	}
+	
+	@GetMapping(value = "/redirect")
+	public String viewTest5() {
+		System.out.println("redirect !");
+		return "redirect:/viewTest1";
 	}
 }
