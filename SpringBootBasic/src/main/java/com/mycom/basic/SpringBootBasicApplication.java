efcc7fc842fbce569b803ced834d13404d2ce835
Min kyungdae <rudwl1005@naver.com>
2022-04-25 13:26:14 +0900

    2022-04-25 SpringBoot (init)

---
+++ SpringBootBasic/src/main/java/com/mycom/basic/SpringBootBasicApplication.java
@@ -0,0 +1,13 @@
+package com.mycom.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBasicApplication.class, args);
+	}
+
+}
---
+++ SpringBootBasic/src/test/java/com/mycom/basic/SpringBootBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycom.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
