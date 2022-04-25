1f1811b4e8ec8d859c5a50a561e6a66e60222c91
Min kyungdae <rudwl1005@naver.com>
2022-04-25 17:30:04 +0900

    2022-04-25 SpringBoot (init)

---
+++ SpringBootDB/src/main/java/com/mycom/mydb/SpringBootDbApplication.java
@@ -0,0 +1,13 @@
+package com.mycom.mydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDbApplication.class, args);
+	}
+
+}
