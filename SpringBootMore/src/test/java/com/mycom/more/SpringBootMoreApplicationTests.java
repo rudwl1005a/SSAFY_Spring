5cf8c08d7da9f2470539c4b06b022eb8c8cbff68
Min kyungdae <rudwl1005@naver.com>
2022-04-27 09:33:43 +0900

    2022-04-27 SpringBoot (init)

---
+++ SpringBootMore/src/test/java/com/mycom/more/SpringBootMoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycom.more;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
