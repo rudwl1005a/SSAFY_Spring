6f48fb88d8351813fc8b65cc385f97213f4acd5e
Min kyungdae <rudwl1005@naver.com>
2022-04-27 14:03:29 +0900

    2022-04-27 SpringBoot (Interceptor)

---
+++ SpringBootMore/src/main/java/com/mycom/more/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.mycom.more.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.mycom.more.common.TestInterceptor;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+	
+	@Autowired
+	private TestInterceptor testInterceptor;
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(testInterceptor)
+			.addPathPatterns("/users/**") // 다른 Controller를 이용해서 다양한 테스트를 하고 있기 때문!
+			.excludePathPatterns("/login/**");
+	}
+}
