18b002d39a63c63820c39baed388a65c9085b6fe
Min kyungdae <rudwl1005@naver.com>
2022-05-13 09:36:03 +0900

    2022-05-13 SpringBoot&Vue (init)

---
+++ BoardWebFileUploadSpringBootMybatisVue/src/main/java/com/mycom/myboard/BoardWebFileUploadSpringBootMybatisVueApplication.java
@@ -0,0 +1,13 @@
+package com.mycom.myboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardWebFileUploadSpringBootMybatisVueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardWebFileUploadSpringBootMybatisVueApplication.class, args);
+	}
+
+}
