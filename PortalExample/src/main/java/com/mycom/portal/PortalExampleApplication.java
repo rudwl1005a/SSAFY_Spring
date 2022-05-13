d446d6dbfdee9f5feaedfc7c480341cec01d71b8
Min kyungdae <rudwl1005@naver.com>
2022-05-13 09:22:04 +0900

    2022-05-13 SpringBoot&Vue (connect)

---
+++ PortalExample/src/main/java/com/mycom/portal/PortalExampleApplication.java
@@ -0,0 +1,13 @@
+package com.mycom.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalExampleApplication.class, args);
+	}
+
+}
---
+++ PortalExample/src/test/java/com/mycom/portal/PortalExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycom.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
